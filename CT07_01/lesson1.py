fd15a99c104f151f215afb19e7854178cd70e0e7
WarrenAndTheTwins <warrenasks@gmail.com>
2025-01-13 01:03:12 +0000

    Jan 13, 2025, 1:03 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,14 @@
 print("Hello from lesson 1")
 
+#check material
+#if glass, slowly lower glass bottle/ picture frame/ other into glass bin
+#if plastic, throw the plastic bottle/ pen/ other into the hoop, OH AND HE SCORES A THREE POINTER!!!!!!
+#if paper, identify if important documents or boogery tissue
+#if important documents, throw away, print "Running from the law?" 
+#if boogery tissue, throw boogery tissue back at person, and print "EWWWW" and turn on siren
+red = 1
+blue = 1
+green = 1
+
+ans = (red * 1 + blue * 2 + green * 3)
+print (ans)

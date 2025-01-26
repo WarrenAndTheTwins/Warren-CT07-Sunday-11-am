fb66385a372a86d5cc164bd872107b3048d7d8f8
WarrenAndTheTwins <warrenasks@gmail.com>
2025-01-26 04:13:42 +0000

    Jan 26, 2025, 4:13 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -3,8 +3,8 @@ print("Hello from lesson 3")
 import time
 
 timer = input("how long is ur study time?")
-while timer > 0:
+while int(timer) > 0:
     print (timer)
     time.sleep(60)
-    timer -= 1
+    int(timer) -= 1
 print("U dumdum, so keep studying.")

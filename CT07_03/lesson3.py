ca82748e7444fbd46d00d8aca99ec52492bbfa7a
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-06 04:29:41 +0000

    Feb 6, 2025, 4:29 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -9,12 +9,13 @@
 #     timer -= 1
 # print("U dumdum, so keep studying.")
 
-savings = 0
+# savings = 0
 
-savings_today = input("How much you save today?")
-savings = savings + float(savings_today)
-while savings < 100:
-    print("You have " + str(savings) + " dollars. You have to save " + str(100 - float(savings)) + " mor monies. You currently have not reached ur goal. You are not cool.")
-    savings_today = input("How much you save today?")
-    savings = savings + float(savings_today)
-print("U suck. Noone likes u. Your free trial has ended, now you must pay " + str(savings) + " dollars.")
+# savings_today = input("How much you save today?")
+# savings = savings + float(savings_today)
+# while savings < 100:
+#     print("You have " + str(savings) + " dollars. You have to save " + str(100 - float(savings)) + " mor monies. You currently have not reached ur goal. You are not cool.")
+#     savings_today = input("How much you save today?")
+#     savings = savings + float(savings_today)
+# print("U suck. Noone likes u. Your free trial has ended, now you must pay " + str(savings) + " dollars.")
+

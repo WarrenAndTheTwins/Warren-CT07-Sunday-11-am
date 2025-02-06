7c79979f83f716049cca3b01beac3b872e415208
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-06 04:55:33 +0000

    Feb 6, 2025, 4:55 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -43,7 +43,7 @@ while life > 0:
 if questions < 15:
     print("YOU NEED TO LEAVE!")
     print(questions)
-    print(str((((questions/(questions + incorrect)) * 100))) + "%")
+    print(str((questions/(questions + incorrect)) * 100) + "%")
 else:
     print("u pretty good!")
 

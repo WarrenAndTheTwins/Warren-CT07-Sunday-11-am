63e1989f0da921c5fd818574d7e5ca3ae8fd9204
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-06 04:24:40 +0000

    Feb 6, 2025, 4:24 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -13,5 +13,5 @@ savings = 0
 while savings < 100:
     savings_today = input("How much you save today?")
     savings = savings + float(savings_today)
-    print("You have " + float(savings) + " dollars. You have to save " + 100 - float(savings) + " mor monies. You currently have not reached ur goal. You are not cool.")
+    print("You have " + str(savings) + " dollars. You have to save " + (100 - float(savings)) + " mor monies. You currently have not reached ur goal. You are not cool.")
 print("U suck. Noone likes u. Your free trial has ended, now you must pay " + str(savings) + " dollars.")

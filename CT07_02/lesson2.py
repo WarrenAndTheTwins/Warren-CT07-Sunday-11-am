a33450172c2bfb339c372fa16de621572388b520
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-20 23:58:40 +0000

    Feb 20, 2025, 11:58 PM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -183,7 +183,7 @@ while True:
                     time.sleep(2) 
                     print("beep boop. Call Ended")
             else:
-                topping_
+                topping_list
 
             
 

c38e39ffc1ae6fd250f623c82cbf1dc301524a6d
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-23 05:04:54 +0000

    Feb 23, 2025, 5:04 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -6,4 +6,4 @@ student1 = ["Jimmy", "12345678", "video games"]
 student2 = ["Sarah", "87654321", "soccer"]
 student3 = ["Ryan", "13572468", "basketball"]
 
-students.append
+students.append(student1)

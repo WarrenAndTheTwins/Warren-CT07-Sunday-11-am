2575391928ef7bd89edc47b69bd31b45affb72fb
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-23 05:15:22 +0000

    Feb 23, 2025, 5:15 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,17 +1,20 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
-students = []
+# students = []
 
-student1 = ["Jimmy", "12345678", "video games"]
-student2 = ["Sarah", "87654321", "soccer"]
-student3 = ["Ryan", "13572468", "basketball"]
+# student1 = ["Jimmy", "12345678", "video games"]
+# student2 = ["Sarah", "87654321", "soccer"]
+# student3 = ["Ryan", "13572468", "basketball"]
 
-students.append(student1)
-students.append(student2)
-students.append(student3)
+# students.append(student1)
+# students.append(student2)
+# students.append(student3)
 
-for student in students:
-    name, phone_number, hobby = student
-    print("Name: " + name)
-    print("Phone Number: " + phone_number)
-    print("Hobby: " + hobby)
+# for student in students:
+#     name, phone_number, hobby = student
+#     print("Name: " + name)
+#     print("Phone Number: " + phone_number)
+#     print("Hobby: " + hobby)
+
+list1 = ["Apple", "Banana", "Cherry"]
+list2 = ["Durian", "Elderberry", "Figs"]

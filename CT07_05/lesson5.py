ff931a17df46d9e5d1dae2ce562590ece19b1228
WarrenAndTheTwins <warrenasks@gmail.com>
2025-02-23 05:01:38 +0000

    Feb 23, 2025, 5:01 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -2,4 +2,4 @@ print("Hello from lesson 5")
 
 student1 = ["Jimmy", "12345678", "video games"]
 student2 = ["Sarah", "87654321", "soccer"]
-student3 = ["Ryan", "13572468"]
+student3 = ["Ryan", "13572468", "basketball"]

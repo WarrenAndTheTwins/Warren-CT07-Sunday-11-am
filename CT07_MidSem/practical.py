e4fe4d8803eada7dbb8f9a6e93ef3093a0c7175f
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-09 03:59:41 +0000

    Mar 9, 2025, 3:59 AM

--- CT07_MidSem/practical.py
+++ CT07_MidSem/practical.py
@@ -22,4 +22,4 @@ while True:
     counter += 1
     ordered_things = ordered_things + counter + ". " + order
     if order == "end":
-        print ("ordered_things")
+        print (ordered_things)

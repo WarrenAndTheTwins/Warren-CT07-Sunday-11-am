a291036dc1965622aff2e9f5b1476696d2e165d5
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-09 03:53:53 +0000

    Mar 9, 2025, 3:53 AM

--- CT07_MidSem/practical.py
+++ CT07_MidSem/practical.py
@@ -5,12 +5,13 @@ print("Jimmy Bartholemew the 2nd Jr Sr has 100 health. He must fight through wav
 health = 100
 counter = 0
 
-#setting up the loop for his repeated attaks
+#setting up the loop for his repeated attacks
 while health > 0:
     health_drop = random.randint(1,15)
     print("OOF! Jimmy Bartholemew the 2nd Jr Sr lost " + str(health_drop) + " fighting a monster!")
     counter += 1
     health = health - health_drop
+
 
 print("Aww, Jimmy Bartholemew the 2nd Jr Sr is drained of health! He survived " + str(counter) + " rounds.")
 

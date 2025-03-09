637fc5ff4aa3fe3c8e1f649e89990470b58addda
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-09 03:34:41 +0000

    Mar 9, 2025, 3:34 AM

--- CT07_MidSem/practical.py
+++ CT07_MidSem/practical.py
@@ -8,4 +8,4 @@ while health > 0:
     health_drop = random.randint(1,15)
     print("OOF! Jimmy Bartholemew the 2nd Jr Sr lost " + health_drop + " fighting a monster!")
     counter += 1
-print("Aww, Jimmy Bartholemew the 2nd Jr Sr is drained of health! He survived ")
+print("Aww, Jimmy Bartholemew the 2nd Jr Sr is drained of health! He survived " + " rounds.")

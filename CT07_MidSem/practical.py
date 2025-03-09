b2bab57a8696f587b8f5916eb39be6370c9aba0e
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-09 03:36:50 +0000

    Mar 9, 2025, 3:36 AM

--- CT07_MidSem/practical.py
+++ CT07_MidSem/practical.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 print("Jimmy Bartholemew the 2nd Jr Sr has 100 health. He must fight through waves of monsters!")
 health = 100
@@ -9,5 +10,5 @@ while health > 0:
     print("OOF! Jimmy Bartholemew the 2nd Jr Sr lost " + str(health_drop) + " fighting a monster!")
     counter += 1
     health == health - health_drop
-    
+
 print("Aww, Jimmy Bartholemew the 2nd Jr Sr is drained of health! He survived " + str(counter) + " rounds.")

dabc7e293742caacf09c31d6a23e8b31acbd0d23
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-09 03:33:24 +0000

    Mar 9, 2025, 3:33 AM

--- CT07_MidSem/practical.py
+++ CT07_MidSem/practical.py
@@ -2,8 +2,10 @@ import random
 
 print("Jimmy Bartholemew the 2nd Jr Sr has 100 health. He must fight through waves of monsters!")
 health = 100
+counter = 0
 
 while health > 0:
     health_drop = random.randint(1,15)
     print("OOF! Jimmy Bartholemew the 2nd Jr Sr lost " + health_drop + " fighting a monster!")
     counter += 1
+print 

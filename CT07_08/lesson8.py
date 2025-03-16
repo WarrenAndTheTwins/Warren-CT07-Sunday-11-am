3661e6b54cc05d357d31544bd3de6f6763a76235
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-16 03:24:58 +0000

    Mar 16, 2025, 3:24 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,20 +1,22 @@
-print("Hello from lesson 8")
+# print("Hello from lesson 8")
 
-list1 = [3, 2, 1]
-list2 = [6, 5, 5]
-list3 = [9, 8, 7]
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
 
-big_list = list1 + list2 + list3
+# big_list = list1 + list2 + list3
 
-big_list = sorted(set(big_list))
-print(big_list)
+# big_list = sorted(set(big_list))
+# print(big_list)
 
-mid_point = len(big_list) // 2
+# mid_point = len(big_list) // 2
 
-right = big_list[mid_point:]
-left = big_list[:mid_point]
-print(right)
-print(left)
+# right = big_list[mid_point:]
+# left = big_list[:mid_point]
+# print(right)
+# print(left)
+
+ps = 
 
 
 

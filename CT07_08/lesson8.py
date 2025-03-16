927a66d9e71f231e90241889d2f9ba87f44f129b
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-16 03:58:55 +0000

    Mar 16, 2025, 3:58 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -15,26 +15,26 @@
 # left = big_list[:mid_point]
 # print(right)
 # print(left)
-HasLower = False
-HasUpper = False
-Mor8 = False
-AlphaNum = False
-HasNum = False
-pw = input("pw plz")
+# HasLower = False
+# HasUpper = False
+# Mor8 = False
+# AlphaNum = False
+# HasNum = False
+# pw = input("pw plz")
 
-if pw.islower: True
-HasLower = True
-if pw.isupper: True
-HasUpper = True
-if len(pw) >= 8: True
-Mor8 = True
-if pw.isalnum: True
-AlphaNum = True
-if pw.isdigit: True
-HasNum = True
+# if pw.islower: True
+# HasLower = True
+# if pw.isupper: True
+# HasUpper = True
+# if len(pw) >= 8: True
+# Mor8 = True
+# if pw.isalnum: True
+# AlphaNum = True
+# if pw.isdigit: True
+# HasNum = True
 
-if HasLower and HasUpper and Mor8 and AlphaNum and HasNum: True
-print(pw)
+# if HasLower and HasUpper and Mor8 and AlphaNum and HasNum: True
+# print(pw)
     
 
 

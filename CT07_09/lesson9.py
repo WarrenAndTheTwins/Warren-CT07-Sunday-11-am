aa15f33f64ae905b4ef32544ac312eb7596a3d83
WarrenAndTheTwins <warrenasks@gmail.com>
2025-03-30 04:16:38 +0000

    Mar 30, 2025, 4:16 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -77,7 +77,7 @@ Plippy.seth(90)
 Plippy.shape("turtle")
 Plippy.color("#9A3836")
 Plippy.goto(0,-250)
-Plippy.write("Bob", align = "center", font =('Futura', 20))
+Plippy.write("Plippy", align = "center", font =('Futura', 20))
 
 window.mainloop()
 

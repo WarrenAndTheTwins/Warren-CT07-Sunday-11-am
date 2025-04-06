6e2c980ae7b11e4a41f937801cd32a8ac440a138
WarrenAndTheTwins <warrenasks@gmail.com>
2025-04-06 03:18:56 +0000

    Apr 6, 2025, 3:18 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -106,6 +106,8 @@ Bippy.pendown()
 while True:
     Plippy.seth(random.randint(75, 155))
     Glob.seth(random.randint(75, 155))
-    Plippy.seth(random.randint(75, 155))
+    Bippy.seth(random.randint(75, 155))
+
+    
 window.mainloop()
 

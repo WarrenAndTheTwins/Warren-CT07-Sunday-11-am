159c78be1646538e77a9340226429cf6a44ae4aa
WarrenAndTheTwins <warrenasks@gmail.com>
2025-04-06 04:17:55 +0000

    Apr 6, 2025, 4:17 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -2,11 +2,13 @@ print("Hello from lesson 10")
 import turtle
 # def alert():
 #     print("MOTION DETECTED")
-t = turtle.Turtle()
+# t = turtle.Turtle()
 
-def square():
-    for i in range (4):
-        t.forward(20)
-        t.left(90)
+# def square():
+#     for i in range (4):
+#         t.forward(20)
+#         t.left(90)
 
+def domath(num1, num2, num3):
+    num1 
     

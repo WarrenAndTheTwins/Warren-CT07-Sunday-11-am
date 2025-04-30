1af8930c20c3e7af94fad1f455ed8bf9212719c0
WarrenAndTheTwins <warrenasks@gmail.com>
2025-04-30 03:57:59 +0000

    Apr 30, 2025, 3:57 AM

--- CT07_11_12_13/revision.py
+++ CT07_11_12_13/revision.py
@@ -1,27 +1,39 @@
-list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
-         5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
-         1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884]
+# list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
+#          5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
+#          1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884]
 
-maximum = 0
-for i in list1:
-    if i > maximum:
-        maximum = i
-print(maximum)
+# maximum = 0
+# for i in list1:
+#     if i > maximum:
+#         maximum = i
+# print(maximum)
 
-# find the minimum
+# # find the minimum
 
-minimum = 10101001010101000101001010010010
-for i in list1:
-    if i < minimum:
-        minimum = i
-print(minimum)
+# minimum = 10101001010101000101001010010010
+# for i in list1:
+#     if i < minimum:
+#         minimum = i
+# print(minimum)
 
-sum = 0
-for i in list1:
-    sum += i
-length = len(list1)
-print(sum)
-average = sum / length
-print(average)
-ARTTSDP = round(sum / length, 2)
-print(ARTTSDP)
+# sum = 0
+# for i in list1:
+#     sum += i
+# length = len(list1)
+# print(sum)
+# average = sum / length
+# print(average)
+# ARTTSDP = round(sum / length, 2)
+# print(ARTTSDP)
+
+
+
+words = [
+    "level", "racecar", "orange", "radar", "banana", "civic", "refer",
+    "apple", "madam", "kayak", "robot", "reviver", "noon", "pop", "deed",
+    "book", "wow", "mirror", "eye", "nope", "rotor", "stats", "hello", 
+    "toot", "peep", "school", "mum", "dad", "gig", "noon",
+    "python", "coding", "class", "student", "lesson", "computer", "keyboard",
+    "monitor", "window", "projector", "teacher", "laptop", "science", "library",
+    "pencil", "marker", "whiteboard", "homework", "question", "answer"
+]

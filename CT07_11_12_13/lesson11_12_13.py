3f893ea1e44ebd5d661c1a99a80e7ea10c90e92e
WarrenAndTheTwins <warrenasks@gmail.com>
2025-04-30 03:25:35 +0000

    Apr 30, 2025, 3:25 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -11,8 +11,40 @@ def initboard():
 
     return board
 
+def printboard(argboard):
+    count = 1
+    for row in argboard:
+        for cell in row:
+            print(f"| {count} ", end="")
+            if count % 3 == 0:
+                print("|")
+                print("-"*13)
+            
+            count += 1
+
+def get_play_move(argboard, player):
+    move = input(f"Player {player}, make move (1-9):")
+
+    if move.isdigit():
+        move = int(move)
+        if move > 0 and move < 10:
+            move -= 1
+            rowofmove = move // 3
+            colofmove = move % 3
+            
+            argboard[rowofmove][colofmove] = player
+
+            return argboard
+        else:
+            print("ONLY 1-9")
+    else:
+        print("NOT NUMBAH")
+
+
 #----------------------------------------------------------------------------------MAIN-GAME-CODE------------------------------------------------------------------------------------------------
 
 board = initboard()
 
-print(board)
+printboard(board)
+
+board = get_play_move(board, "X")

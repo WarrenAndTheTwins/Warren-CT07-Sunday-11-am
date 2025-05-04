0dab3e6bfdd1dc8e97aaf080b796a4a346f4d038
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-04 03:22:53 +0000

    May 4, 2025, 3:22 AM

--- practical.py
+++ practical.py
@@ -30,8 +30,8 @@ daily_sales = [1205, 986, 1354, 10535, 15741, 11200, 800,
 sum = 0
 average = 0
 maximum_sale = max(daily_sales)
-ma
-print("August" + maximum_sale.index() + 1 + maximum_sale)
+maximum_day = maximum_sale.index() + 1
+print("August" + maximum_sale.index() +  + maximum_sale)
 minimum_sale = min(daily_sales)
 print(minimum_sale)
 for day_sale in daily_sales:

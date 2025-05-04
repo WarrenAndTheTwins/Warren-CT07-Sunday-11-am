21f71052ca9cd7900339c755d795333ea2e8ffc5
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-04 03:39:41 +0000

    May 4, 2025, 3:39 AM

--- practical.py
+++ practical.py
@@ -84,7 +84,7 @@ list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
 for i in list1:
     is_even(i)
     if is_even == True:
-        print("Yep, " + "") 
+        print("Yep, " + i + "is even") 
     if False:
         print("Nope,"  + i + " is not even")
     

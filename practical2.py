09e63e9d7df9622c4c191aaafd3e971e73a60a31
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-04 03:44:59 +0000

    May 4, 2025, 3:44 AM

--- practical2.py
+++ practical2.py
@@ -27,8 +27,7 @@ list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
 
 ###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
 for i in list1:
-    is_even(i)
-    if is_even == True:
+    if is_even(i) == True:
         print("Yep, " + i + "is even") 
     if is_even == False:
         print("Nope,"  + i + " is not even")

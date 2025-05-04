12fa421411b4937ce82d1d0840dd408c8048d7bf
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-04 03:34:04 +0000

    May 4, 2025, 3:34 AM

--- practical.py
+++ practical.py
@@ -39,3 +39,44 @@ for day_sale in daily_sales:
     sum += day_sale 
     average = round(sum / len(daily_sales), 2)
 print("The average sale per day in August was " + str(average))
+
+# You are given a list of 100 random numbers. 
+# Your task is to check if each number is odd or even.
+
+# To check if the number is odd or even 
+# Write a function to determine if a number is even.
+# if number is even, return True, else return False.
+
+# Example Output:
+# 2944 is even.
+# 5490 is even.
+# 2357 is odd.
+# 2619 is odd.
+
+
+##### Task 1: COMPLETE THIS FUNCTION TO CHECK ODD/ EVEN
+def is_even(num):
+
+    pass # remove this when you code. Ask Code Mentor if unsure.
+    # Write your code to check if the number is even
+    
+    # return True or False
+
+# Example output:
+list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
+         5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
+         1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884,
+         756, 6196, 5935, 5291, 8619, 2630, 1831, 3127, 4698, 6291,
+         2478, 5792, 9362, 7348, 8040, 3556, 598, 6187, 8959, 880,
+         6601, 538, 3439, 8508, 8649, 5139, 8076, 78, 6776, 362,
+         6368, 6460, 8604, 1763, 1713, 2354, 2167, 6612, 8149, 7961,
+         4270, 5285, 7346, 5667, 2102, 900, 8063, 4577, 2285, 9592,
+         5671, 537, 9777, 9421, 5455, 1241, 990, 3745, 8443, 4213,
+         4183, 2463, 9562, 8137, 5101, 397, 6966, 9927, 7473, 4105]
+
+###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
+for i in list1:
+    pass # remove this when you code. Ask Code Mentor if unsure.
+
+    # call the function to check if the number is odd or even
+

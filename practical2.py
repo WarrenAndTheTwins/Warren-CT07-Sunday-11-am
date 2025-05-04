3571e5308b17d97fc6a5364bab4ebd54b3067aec
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-04 03:42:58 +0000

    May 4, 2025, 3:42 AM

---
+++ practical2.py
@@ -0,0 +1,38 @@
+
+##### Task 1: COMPLETE THIS FUNCTION TO CHECK ODD/ EVEN
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+      
+    # remove this when you code. Ask Code Mentor if unsure.
+
+    # Write your code to check if the number is even
+    
+    # return True or False
+
+# Example output:
+list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
+         5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
+         1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884,
+         756, 6196, 5935, 5291, 8619, 2630, 1831, 3127, 4698, 6291,
+         2478, 5792, 9362, 7348, 8040, 3556, 598, 6187, 8959, 880,
+         6601, 538, 3439, 8508, 8649, 5139, 8076, 78, 6776, 362,
+         6368, 6460, 8604, 1763, 1713, 2354, 2167, 6612, 8149, 7961,
+         4270, 5285, 7346, 5667, 2102, 900, 8063, 4577, 2285, 9592,
+         5671, 537, 9777, 9421, 5455, 1241, 990, 3745, 8443, 4213,
+         4183, 2463, 9562, 8137, 5101, 397, 6966, 9927, 7473, 4105]
+
+###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
+for i in list1:
+    is_even(i)
+    if is_even == True:
+        print("Yep, " + i + "is even") 
+    if is_even == False:
+        print("Nope,"  + i + " is not even")
+    
+    # remove this when you code. Ask Code Mentor if unsure.
+
+    # call the function to check if the number is odd or even

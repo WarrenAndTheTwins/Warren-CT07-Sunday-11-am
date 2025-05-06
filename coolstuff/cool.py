642a1593d3643c8d1003fc968f6ae36dd9604195
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-06 04:56:06 +0000

    May 6, 2025, 4:56 AM

--- coolstuff/cool.py
+++ coolstuff/cool.py
@@ -5,5 +5,8 @@ def guessgame(min, max):
     ans = random.randint(min, max)
     guess = input("Guess a number between " + min + " and " + max + ".")
     if ans == guess:
-        return True\
+        return True
+    else:
+        return False
+guessgame()
 

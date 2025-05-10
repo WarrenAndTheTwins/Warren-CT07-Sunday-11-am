3c352cdb31abe8a393b2a3cd004b412250615e26
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-10 04:33:58 +0000

    May 10, 2025, 4:33 AM

--- coolstuff/cool.py
+++ coolstuff/cool.py
@@ -40,6 +40,6 @@ while True:
             print("Incorrect")
             streak = 0
     elif difficulty in ["streak", "Streak"]:
-        print("Your streak is at " + streak + ".")
+        print("Your streak is at " + str(streak) + ".")
     else: 
         print("Sorry, choose something else.")

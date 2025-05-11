810f3aa91a826716014c355b298cf60654d1295e
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-11 22:48:22 +0000

    May 11, 2025, 10:48 PM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -36,7 +36,7 @@ else:
         if input("No, no, it can't be..... It can't be gun!") == "krn8t6":
             print("Access unlocked.")
             time.sleep(2)
-            input()
+            input("Rocch")
     else: 
         print("Hmmm... I've never seen that played before.")
     

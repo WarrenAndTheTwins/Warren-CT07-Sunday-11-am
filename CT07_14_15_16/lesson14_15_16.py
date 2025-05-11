4840af1305acd2cb173fd1ab618835f72c6581d3
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-11 22:46:59 +0000

    May 11, 2025, 10:46 PM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -1,1 +1,47 @@
 print("Hello from lesson 14_15_16")
+import pygame
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+paddle_width = 20
+paddle_height = 100
+paddle1_x = 10
+paddle1_y = screen_height // 2 - paddle_height // 2
+paddle2_x = screen_width - paddle_width - 10
+paddle2_y = screen_height // 2 - paddle_height // 2
+ball_radius = 10
+ball_x = screen_width // 2
+ball_y = screen_height // 2
+white = (255, 255, 255)
+ball_dx = 0.5
+ball_dy = 0.5
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Pong Game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w] and paddle1_y > 0:
+        paddle1_y -= 1
+    if keys[pygame.K_s] and paddle1_y < screen_height - paddle_height:
+        paddle1_y += 1
+
+    if keys[pygame.K_UP] and paddle1_y > 0:
+        paddle1_y -= 1
+    if keys[pygame.K_DOWN] and paddle1_y < screen_height - paddle_height:
+        paddle1_y += 1
+    ball_x += ball_dx
+    ball_y += ball_dy
+
+    pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
+    pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
+    pygame.draw.circle(screen, white, (ball_x, ball_y), ball_radius)
+    pygame.display.flip()
+pygame.quit
---
+++ coolstuff/rps.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+print("Long ago, in a land where balance reigned, the mighty spirits of Rock, Paper, and Scissors engaged in their endless battle for supremacy.=> Each had their strength—Rock crushed Scissors, Scissors cut Paper, and Paper covered Rock. No one could dominate entirely, ensuring a perfect cycle.<= Then, one day, someone whispered of a new force… Gun. For a brief moment, chaos erupted. The three ancient spirits gasped in horror. Then Paper—quick-witted and crafty—simply wrapped around Gun, silencing it forever. And so, Rock, Paper, and Scissors continued their eternal dance, their legend untarnished. Gun? Well, it was never officially part of the game, and we don’t talk about that awkward moment.<= Move along. 😆")
+print(" ")
+choice = input("Rock, Paper, Scissors.....")
+
+robot_choice = random.randint(0, 2)
+coollist = ["rock", "paper", "scissors"]
+final_choice = coollist[robot_choice]
+
+if choice in ["rock", "Rock", "r", "R", "paper", "Paper", "p", "P", "Scissors", "scissors", "s", "S"]:
+    print("Shoot!")
+    time.sleep(1)
+    print("I picked " + str(final_choice) + "!")
+    time.sleep(0.5)
+    if final_choice == "rock" and choice in ["Scissors", "scissors", "s", "S"]:
+        print("Nice! GGs, I win!")
+    elif final_choice == "paper" and choice in ["rock", "Rock", "r", "R"]:
+        print("Nice! GGs, I win!")
+    elif final_choice == "scissors" and choice in ["paper", "Paper", "p", "P"]:
+        print("Nice! GGs, I win!")
+    elif final_choice == "scissors" and choice in ["Scissors", "scissors", "s", "S"]:
+        print("Oh, a tie. Nice job!")
+    elif final_choice == "paper" and choice in ["paper", "Paper", "p", "P"]:
+        print("Oh, a tie. Nice job!")
+    elif final_choice == "rock" and choice in ["rock", "Rock", "r", "R"]:
+        print("Oh, a tie! Nice job!")
+    else:
+        print("Wow! You win!")
+else:
+    if choice in ["gun", "GUN"]:
+        print("Really? That's quite immature. *drinks tea, pinky sticking out*")
+    if choice in ["><<", "=><==<", "=> <= =>"]:
+        print ("Huh? What's tha-k̸̯̦͝r̶̢̦̈́̐n̷̯̼͑8̵̖͐͠t̶̻̎̋6̸̛̘̖̑?̷͎̒́")
+        if input("No, no, it can't be..... It can't be gun!") == "krn8t6":
+            print("Access unlocked.")
+            time.sleep(2)
+    else: 
+        print("Hmmm... I've never seen that played before.")
+    

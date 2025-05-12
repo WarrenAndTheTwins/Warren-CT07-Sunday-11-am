62c18ba4975e6566f5613530f4beb53abeb3a945
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:32:18 +0000

    May 12, 2025, 4:32 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -76,7 +76,7 @@ else:
                     print("I lose...")
                     time.sleep(2)
                     print("*he walks away, defeated*")
-                    print()
+                    print("")
 
     else: 
         print("Hmmm... I've never seen that played before.")

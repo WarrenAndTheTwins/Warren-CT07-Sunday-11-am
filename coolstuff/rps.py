94c1e93e80757c61ea16a2a2d3f1b21721139e8c
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:20:16 +0000

    May 12, 2025, 4:20 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -51,7 +51,8 @@ else:
                 print("But, a wise choice is not always a good one.")
                 time.sleep(1)
                 print("*he spins around, showing you his choice.")
-                print("I choose gun. Shatters rock, pierces paper, ")
+                tinm
+                print("I choose gun. Shatters rock, pierces paper, cripples scissors.")
     else: 
         print("Hmmm... I've never seen that played before.")
     

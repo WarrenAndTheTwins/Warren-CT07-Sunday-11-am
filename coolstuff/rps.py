67f551b5794281b0bf9c63cd767e14e2f667218b
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:16:57 +0000

    May 12, 2025, 4:16 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -44,7 +44,7 @@ else:
             if fun == "1":
                 print("Ah, finally, someone who recognizes me!")
                 print("I've been cast out for a long time. Finally, someone knows me.")
-                print("Let's play rocch, p")
+                print("Let's play rocch, peiber, cissurs")
     else: 
         print("Hmmm... I've never seen that played before.")
     

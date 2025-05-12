3e2c8271294b24d885ec49a9743b6eab90e634eb
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:33:04 +0000

    May 12, 2025, 4:33 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -77,7 +77,7 @@ else:
                     time.sleep(2)
                     print("*he walks away, defeated*")
                     print("Ending 3! Also, happy mother's day!")
-                if fun in ["scissors", "cissurs", "Scissors", "Cissurs", "S", "s", "C", "c"]:
+                if fun in ["rock", "cissurs", "Scissors", "Cissurs", "S", "s", "C", "c"]:
                     print("I played paper... ")
                     time.sleep(1)
                     print("I lose...")

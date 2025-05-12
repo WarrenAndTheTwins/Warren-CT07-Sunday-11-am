86e3821b03346ed530acc1835059a3e81f0db65e
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:29:07 +0000

    May 12, 2025, 4:29 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -69,7 +69,8 @@ else:
                 time.sleep(1)
                 print("...and I fixed that...")
                 time.sleep(1)
-                print("Let's pla")
+                print("Let's play rocch, peiber, cissurs...")
+                
     else: 
         print("Hmmm... I've never seen that played before.")
     

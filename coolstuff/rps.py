4ecc983a6ddb1b8bb76a46e69d968ae9a8dc0849
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:26:06 +0000

    May 12, 2025, 4:26 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -65,6 +65,7 @@ else:
             if fun == "2":
                 print("Hmm, the one who doesn't know...")
                 time.sleep(2)
+                print("Well, ")
     else: 
         print("Hmmm... I've never seen that played before.")
     

70355b75f79fae395ab9917f3399251476d932ee
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-12 04:14:03 +0000

    May 12, 2025, 4:14 AM

--- coolstuff/rps.py
+++ coolstuff/rps.py
@@ -38,7 +38,7 @@ else:
             time.sleep(2)
             print("Rocch, peibur, cissurs.")       
             print("say: Is that you, gun? by typing 1")
-            print("say: What do you mean by rocch, ")                            
+            print("say: What do you mean by rocch, peibur, cissurs? by typing 2")                            
     else: 
         print("Hmmm... I've never seen that played before.")
     

27e902a9175a2db98cc7ce2b6d546a01779612fa
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-18 04:36:02 +0000

    May 18, 2025, 4:36 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -64,7 +64,7 @@ while running:
     screen.blit(tennis_ball_image, (ball_x - ball_radius, ball_y - ball_radius))
     player1_score_text = score_font.render("Player 1 : " + str(player1_score), True, (0, 0, 0))
     player1_score_text = score_font.render("Player 2 : " + str(player2_score), True, (0, 0, 0))
-    screen.blit(player1)
+    screen.blit(player1_score_text, ())
 
     pygame.display.flip()
 pygame.quit()

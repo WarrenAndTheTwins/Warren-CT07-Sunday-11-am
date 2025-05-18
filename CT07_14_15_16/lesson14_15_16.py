5a1cc88b66f8928ac7bfa822e4718194fb34de3c
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-18 04:05:01 +0000

    May 18, 2025, 4:05 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -56,6 +56,6 @@ while running:
 
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
-    pygame.draw.circle(screen, white, (ball_x, ball_y), ball_radius)
+    pygame.blit
     pygame.display.flip()
 pygame.quit()

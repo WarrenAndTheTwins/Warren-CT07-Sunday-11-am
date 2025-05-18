8842e2cb5cd206ac8193892d5d4d150eddd4232d
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-18 03:42:47 +0000

    May 18, 2025, 3:42 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -47,7 +47,7 @@ while running:
     paddle1_box = pygame.Rect(paddle1_x, paddle1_y, paddle_width, paddle_height)
     paddle2_box = pygame.Rect(paddle2_x, paddle2_y, paddle_width, paddle_height)
 
-    if ball_x <= paddle1_box.right + ball_radius and ball_y >= paddle1_box.top and ball_y <= padd:
+    if ball_x <= paddle1_box.right + ball_radius and ball_y >= paddle1_box.top and ball_y <= paddle1_box.bottom:
         ball_dx *= -1
     if ball_x <= paddle2_box.left + ball_radius and ball_y >= paddle1_box.top:
         ball_dx *= -1           

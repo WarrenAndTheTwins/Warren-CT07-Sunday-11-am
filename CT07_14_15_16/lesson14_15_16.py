12a5b2d645d5b990f7461249aaf7c6fb1bc687dd
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-18 04:12:18 +0000

    May 18, 2025, 4:12 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -54,6 +54,7 @@ while running:
     if ball_x >= paddle2_box.left - ball_radius and ball_y >= paddle2_box.top and ball_y <= paddle2_box.bottom:
         ball_dx *= -1           
 
+    screen.blit()
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
     screen.blit(tennis_ball_image, (ball_x - ball_radius, ball_y - ball_radius))

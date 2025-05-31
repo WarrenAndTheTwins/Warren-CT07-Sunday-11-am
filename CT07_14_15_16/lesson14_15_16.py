9449ebbb432db691e8d42fe8ecac08395dc0793a
WarrenAndTheTwins <warrenasks@gmail.com>
2025-05-31 18:00:51 +0000

    May 31, 2025, 6:00 PM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -36,14 +36,14 @@ while running:
     keys = pygame.key.get_pressed()
     screen.blit(background_image,(0,0)) #this set the color back black 
     if keys[pygame.K_w] and paddle1_y > 0:
-        paddle1_y -= 1
+        paddle1_y -= 3
     if keys[pygame.K_s] and paddle1_y < screen_height - paddle_height:
-        paddle1_y += 1
+        paddle1_y += 3
 
     if keys[pygame.K_UP] and paddle2_y > 0:
-        paddle2_y -= 1
+        paddle2_y -= 3
     if keys[pygame.K_DOWN] and paddle2_y < screen_height - paddle_height:
-        paddle2_y += 1
+        paddle2_y += 3
     ball_x += ball_dx
     ball_y += ball_dy
 
